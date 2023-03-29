599ab0b01dcb3f58ec255d2343dfc403c357d2ed
pipecraft <pipecraft.net@gmail.com>
2023-03-29 09:13:07 +0800

    0.0.4

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,14 +1,47 @@
 export function uniq(array: any[]): any[]
+
 export function toCamelCase(text: string): string
-export function querySelector(element, selectors?: string): HTMLElement
-export function querySelectorAll(element, selectors?: string): any[]
-export function $(element, selectors?: string): HTMLElement
-export function $$(element, selectors?: string): any[]
-export function createElement(type: string): HTMLElement
-export function addEventListener(element, type, listener): void
-export function setAttribute(element, attr, value): void
-export function setStyle(element, style): void
+
+export function $(
+  element: HTMLElement | string,
+  selectors?: string
+): HTMLElement
+
+export function querySelector(
+  element: HTMLElement | string,
+  selectors?: string
+): HTMLElement
+
+export function $$(
+  element: HTMLElement | string,
+  selectors?: string
+): HTMLElement[]
+
+export function querySelectorAll(
+  element: HTMLElement | string,
+  selectors?: string
+): HTMLElement[]
+
+export function createElement(tagName: string): HTMLElement
+
+export function addEventListener(
+  element: HTMLElement,
+  type: string | Record<string, unknown>,
+  listener?
+): void
+
+export function setAttribute(
+  element: HTMLElement,
+  name: string,
+  value: string
+): void
+
+export function setStyle(element, style: string | Record<string, unknown>): void
+
 export function toStyleMap(styleText: string): Record<string, unknown>
+
 export function noStyleSpace(text: string): string
+
 export function createSetStyle(styleText: string): setStyle
+
 export function isUrl(text: string): boolean
