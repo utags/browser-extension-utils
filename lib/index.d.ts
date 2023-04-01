6e3da411c14ecd62146002f63ea71ab3b4641849
pipecraft <pipecraft.net@gmail.com>
2023-04-02 01:50:58 +0800

    Add addElement, addStyle

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -2,33 +2,62 @@ export function uniq(array: any[]): any[]
 
 export function toCamelCase(text: string): string
 
+export function $(selectors: string): HTMLElement
+
 export function $(
   element: HTMLElement | Document | string,
-  selectors?: string
+  selectors: string
 ): HTMLElement
+
+export function querySelector(selectors: string): HTMLElement
 
 export function querySelector(
   element: HTMLElement | Document | string,
-  selectors?: string
+  selectors: string
 ): HTMLElement
+
+export function $$(selectors: string): HTMLElement[]
 
 export function $$(
   element: HTMLElement | Document | string,
-  selectors?: string
+  selectors: string
 ): HTMLElement[]
+
+export function querySelectorAll(selectors: string): HTMLElement[]
 
 export function querySelectorAll(
   element: HTMLElement | Document | string,
-  selectors?: string
+  selectors: string
 ): HTMLElement[]
 
-export function createElement(tagName: string): HTMLElement
+export function createElement(
+  tagName: string,
+  attributes?: Record<string, unknown>
+): HTMLElement
+
+export function addElement(
+  tagName: string,
+  attributes?: Record<string, unknown>
+): HTMLElement
+
+export function addElement(
+  parentNode: HTMLElement,
+  tagName: string,
+  attributes?: Record<string, unknown>
+): HTMLElement
+
+export function addStyle(styleText: string): HTMLElement
 
 export function addEventListener(
   element: HTMLElement | Document | EventTarget,
-  type: string | Record<string, unknown>,
-  listener?
-): void
+  type: string,
+  listener: Function
+): () => void
+
+export function addEventListener(
+  element: HTMLElement | Document | EventTarget,
+  type: string | Record<string, unknown>
+): () => void
 
 export function getAttribute(element: HTMLElement, name: string): string
 
